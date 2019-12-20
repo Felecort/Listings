df0b0b350b79d99eb29ee258b5023f682abc0a72
FriLDD <dotpointen@gmail.com>
2019-12-20 18:44:14 +0300

    vk bot

---
+++ Bot_VK/options.py
@@ -0,0 +1,20 @@
+#ID страницы
+acc_id = ''
+
+#Токен страницы (Kate mobile):
+vk = Vk(token='')
+
+#Вывод ошибок (Число)
+#err_id = 0 - не выводить ошибки
+#err_id = acc_id - себе в лс
+#err_id = ... Как получить id диалога: /send(peer_id)
+err_id = 2000000174
+
+#Настройка QIWI
+qiwitoken = 'токен киви'
+qiwinum = 'номер киви'
+
+#Токен Яндекс переводчика https://translate.yandex.ru/developers/keys
+ytoken = 'токен Яндекс'
+yt = YTranslator(token=ytoken)
+
